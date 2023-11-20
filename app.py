fad0440f5035b9a3294ef614a9cea11bef2d9e10
riddhima-7321 <132067601+riddhima-7321@users.noreply.github.com>
2023-11-21 02:44:05 +0530

    wine_model

---
+++ app.py
@@ -0,0 +1,19 @@
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    int_features = [x for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features).astype(int)
+
+    return render_template('index.html', prediction_text='QUALITY OF WINE IS:{}'.format(prediction))
+
+if __name__ == "__main__":
+    app.run(debug=True)
